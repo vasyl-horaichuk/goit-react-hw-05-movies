953878b43cd5f8623b7e13b460786ca1e7319051
Vasyl Horaichuk <vg.createam@gmail.com>
2023-02-12 23:23:45 +0200

    handle

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,14 +1,32 @@
+import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
 export const Movies = () => {
+  const [query, setQuery] = useState('');
+
+  const data = useSearchParams();
+  console.log(data);
+
+  const handleChange = e => {
+    setQuery(e.targe.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <h1>TITLE</h1>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <label htmlFor="">
           <input
             type="text"
             autoComplete="off"
             autoFocus
             placeholder="Search movies"
+            value={query}
+            onChange={handleChange}
           />
         </label>
         <button type="submit">submit</button>
