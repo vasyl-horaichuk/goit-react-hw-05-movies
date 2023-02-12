f2216db00e97ecbe1b4a5076b9fa4f37c688a4c5
Vasyl Horaichuk <vg.createam@gmail.com>
2023-02-12 15:23:24 +0200

    service fetch make

--- src/service/fetchMovies.js
+++ src/service/fetchMovies.js
@@ -3,25 +3,59 @@ import axios from 'axios';
 const API_KEY = '740dee1bffaa9bd60e4b0e1462b3e0c6';
 const URL = 'https://api.themoviedb.org/3/';
 
+const params = {
+  search: '&language=ua-UK&page=1&include_adult=false',
+  language: '&language=ua-UK',
+};
+
 axios.defaults.params = {
   baseURL: URL,
   key_api: API_KEY,
 };
 
-const fetchMoviesTrending = async () => {
+export const fetchMoviesTrending = async () => {
   try {
     const responce = await axios.get(`trending/all/day`);
-    // const responce = await axios.get(`trending/all/day?api_key=${params.key}`);
     return responce.date;
   } catch (error) {
     console.log('ERROR: ' + error);
   }
 };
 
-const fetchSearchMovies = async () => {};
+export const fetchSearchMovies = async qeury => {
+  try {
+    const responce = await axios.get(
+      `/search/movie${params.search}&query=${qeury}`
+    );
+    return responce.results;
+  } catch (error) {
+    console.log('ERROR: ' + error);
+  }
+};
 
-const fetchMovieDetails = async () => {};
+export const fetchMovieDetails = async id => {
+  try {
+    const responce = await axios.get(`/movie/${id}${params.language}`);
+    return responce.date;
+  } catch (error) {
+    console.log('ERROR: ' + error);
+  }
+};
 
-const fetchMovieCredits = async () => {};
+export const fetchMovieCredits = async id => {
+  try {
+    const responce = await axios.get(`/movie/${id}/credits${params.language}`);
+    return responce.cast;
+  } catch (error) {
+    console.log('ERROR: ' + error);
+  }
+};
 
-const fetchMovieReviews = async () => {};
+export const fetchMovieReviews = async id => {
+  try {
+    const responce = await axios.get(`/movie/${id}/credits${params.language}`);
+    return responce.results;
+  } catch (error) {
+    console.log('ERROR: ' + error);
+  }
+};
