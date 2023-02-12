535422209fec72fce39d00034aab6adb563e3125
Vasyl Horaichuk <vg.createam@gmail.com>
2023-02-12 13:23:12 +0200

    axios make

---
+++ src/service/fetchMovies.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+// const API_KEY = '740dee1bffaa9bd60e4b0e1462b3e0c6';
+const params = {
+  key: '740dee1bffaa9bd60e4b0e1462b3e0c6',
+};
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+
+const fetchMoviesTrending = async () => {
+  try {
+    const responce = await axios.get(`trending/all/day?api_key=${params.key}`);
+    return responce.date;
+  } catch (error) {
+    console.log('ERROR: ' + error);
+  }
+};
+console.log(response);
+
+fetchMoviesTrending().then(responce => lonsole.log(responce));
+
+const fetchSearchMovies = async () => {};
+
+const fetchMovieDetails = async () => {};
+
+const fetchMovieCredits = async () => {};
+
+const fetchMovieReviews = async () => {};
