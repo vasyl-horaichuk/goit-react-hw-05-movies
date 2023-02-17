9a42967f6f7a447ed9ca04ad7c03238549130f99
Vasyl Horaichuk <vg.createam@gmail.com>
2023-02-17 19:12:45 +0200

    svsd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 
 const Home = lazy(() => import('../pages/Home'));
