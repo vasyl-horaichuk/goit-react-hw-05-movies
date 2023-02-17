14c96e8f90df5e689a66a64e8be92e07db519604
Vasyl Horaichuk <vg.createam@gmail.com>
2023-02-17 20:23:31 +0200

    sefse

--- src/components/Movies/moviesList.jsx
+++ src/components/Movies/moviesList.jsx
@@ -1,6 +1,6 @@
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
-import PropTypes from 'prop-types';
 
 export const MoviesList = ({ movies }) => {
   const location = useLocation();
